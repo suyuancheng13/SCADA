e9780aea5ef4ad16d4aba21f8b92916c533437aa
yuancheng su <suyauncheng13@163.com>
2013-09-07 09:31:55 +0800

    create  a mfc project

---
+++ SCADA/SCADA/SCADA.h
@@ -0,0 +1,40 @@
+
+// SCADA.h : SCADA 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CSCADAApp:
+// 有关此类的实现，请参阅 SCADA.cpp
+//
+
+class CSCADAApp : public CWinAppEx
+{
+public:
+	CSCADAApp();
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	UINT  m_nAppLook;
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSCADAApp theApp;
